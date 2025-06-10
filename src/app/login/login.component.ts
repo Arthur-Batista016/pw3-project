ebfefd50897f75767fafdf16a81965638b0126aa
Arthur-Batista016 <arthurbonvecchiobatista@gmail.com>
2025-06-10 11:03:26 -0300

    feat: login page started

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { AbstractControl, Form, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+
+})
+export class LoginComponent {
+
+  user:string = 'tadeu';
+  password:number = 1313;
+
+
+  form: FormGroup;
+
+
+  constructor(private fb:FormBuilder) {
+      this.form = this.fb.group({
+        username:['',Validators.required],
+        passwords: this.fb.group({
+          password:['',Validators.required],
+          confirmPassword:['',Validators.required]
+        },
+        {validators: this.mesmasenhaValidator})
+      });
+  }
+
+
+  cadastrar(){
+    if(this.form.valid){
+      alert("Login Efetuado com sucesso!")
+    }else{
+      alert("Login Falhou !")
+    }
+  }
+
+    mesmasenhaValidator:ValidatorFn = (formGroup:AbstractControl):ValidationErrors | null =>{
+    let senha = formGroup.get('password')?.value;
+    let confirmarSenha = formGroup.get('confirmPassword')?.value;
+
+    if(senha !== confirmarSenha){
+      return {senhasDiferentes:true}
+    }
+
+     return null;
+
+  }
+
+}
+// verifyLogin:ValidatorFn = (FormGroup:AbstractControl):ValidationErrors | null =>{
+  //   let username = this.form.get('username')?.value
+  //   let pass = this.form.get('passwords')?.get("password")?.value
+  //   if(this.user!=username && this.password != pass){
+  //     return {loginFailed:true}
+  //   }
