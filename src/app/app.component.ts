1f04902de9db8646c70f46bc22c859a9717de7dd
Arthur-Batista016 <arthurbonvecchiobatista@gmail.com>
2025-06-16 22:27:16 -0300

    Feat: Home page Started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
+import { HomePageComponent } from './home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
